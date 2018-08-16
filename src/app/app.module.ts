a49377564869053e0916c85c2738c98bae0a549b
Chebrolu <chebrve@repsrv.com>
2018-08-16 20:49:03 +0530

    Initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { productListComponent } from './products/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    productListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
